2cdda85e92d21c7a3154a1d24113a8ca95455016
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-09 11:53:41 +0530

    Add bcrypt for password hashing in user creation
    Integrated bcrypt to hash user passwords when creating a new user in userController.js. Updated user creation logic to store hashed passwords instead of plain text. Added bcrypt and its dependencies to the project.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
 import User from "../models/user.js"; // Import User model
+import bcrypt from "bcrypt"; // Import bcrypt for password hashing
 
 export function createUser(req, res) { 
+
+    const hashPassword = bcrypt.hashSync(req.body.password, 10); // Hash the password with a salt round of 10   
+
     const user = new User(
-        req.body
-    )
+        {
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hashPassword,
+            role: req.body.role
+        }
+    ) 
 
     user.save().then(
         () => {
