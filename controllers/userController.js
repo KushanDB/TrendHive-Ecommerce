164e8a1233e662ecaafc42a0525cc4e7ca183b83
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-09 15:02:02 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,29 +34,31 @@ export function createUser(req, res) {
     )
 }
 
-export function loginUser(req, res) { // Login function
+// Handle user login: Check if email exists, then verify password matches 
 
-    User.findOne( // Find user by email
+export function loginUser(req, res) {  // Login user function
+
+    User.findOne( // Find user by email from the request body in database
         { 
             email: req.body.email 
         }
     ).then(
-        (user) => {
-            if (user == null) {
+        (user) => {                     // Callback function after finding user
+            if (user == null) {    // If user not found
                 res.json(
                     {
                         message: "User not found"
                     }
                 )
-            }else{
+            }else{   // If user found                    
                 const isPasswordValid = bcrypt.compareSync(req.body.password, user.password); // Compare the provided password with the stored hashed password
-                if (isPasswordValid) {
+                if (isPasswordValid) { // If password matches
                     res.json(
                         {
                             message: "Login successful"
                         }
                     )
-                }else{
+                }else{ // If password does not match
                     res.json(
                         {
                             message: "Invalid password"
