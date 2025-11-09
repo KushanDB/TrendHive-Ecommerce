a9fbc5b14402d92f89189a6d6cf3a6e9c69c4482
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-09 10:22:01 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema( // structure of the user document 
+    {
+        email: {
+            type: String,
+            required: true, // email is required
+            unique: true // email must be unique
+        },
+        firstName: {
+            type: String,
+            required: true // first name is required
+        },
+        lastName: {
+            type: String,
+            required: true // last name is required
+        },
+        password: {
+            type: String,
+            required: true // password is required
+        },
+        role: {
+            type: String,
+            required: true, // user type is required
+            default: "user" // default role is 'user'
+        },
+        isBlock: {
+            type: Boolean,
+            default: false // default is not blocked
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false // default is not verified
+        },
+        image: {
+            type: String,
+            default: "#######" // default image is empty
+        }
+    }
+) 
+
+const User = mongoose.model("User", userSchema); // Define Mongoose Model
+export default User; // Export User model
