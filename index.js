a29af4eff350fa07b4f73394469d6698c8475fdc
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-12 18:10:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mongoose from "mongoose";  
+import mongoose from "mongoose";
 import studentRouter from "./routes/studentsRouter.js"; // Import studentRouter
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken"; // Import jsonwebtoken for token verification
@@ -18,15 +18,15 @@ app.use(express.json())
 
 // Custom middleware to log HTTP requests
 app.use(
-    (req,res,next)=>{
+    (req, res, next) => {
         let token = req.header("Authorization");
         //console.log("Token received in middleware: ", token);
         //console.log("Http request has been received to the middleware...")
-        if(token != null){
-            token = token.replace("Bearer ",""); // Remove 'Bearer ' prefix if present
-            jwt.verify(token,"jwt secretkey", // decrypt the token to get user data
-                (err,decoded)=>{
-                        console.log("Decoded token data:", decoded);
+        if (token != null) {
+            token = token.replace("Bearer ", ""); // Remove 'Bearer ' prefix if present
+            jwt.verify(token, "jwt secretkey", // decrypt the token to get user data
+                (err, decoded) => {
+                    console.log("Decoded token data:", decoded);
                 }
             )
         }
@@ -36,11 +36,11 @@ app.use(
 const connectionString = "mongodb+srv://admin:Kushan123@cluster0.lloh3q9.mongodb.net/?appName=Cluster0"
 
 mongoose.connect(connectionString).then(
-    ()=>{
+    () => {
         console.log("Database connected...")
     }
 ).catch(
-    ()=>{
+    () => {
         console.log("Database connection Failed...")
     }
 )
@@ -57,8 +57,8 @@ app.use("/users", userRouter)
 // Start the server in port 5000..
 
 app.listen(5000,
-    ()=>{
+    () => {
         console.log("Server is started...")
         console.log("You can proceed to your work...")
     }
-    )
+)
