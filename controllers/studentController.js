bd95ae450965318cb072916f663da35b6667eb88
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-13 10:55:14 +0530

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -43,6 +43,8 @@ export function createStudent(req,res){
         return; // Stop further processing if user is not admin
     }
 
+    //-------------------create student object-----------------
+
         const student = new Student(
             {
                 name : req.body.name,
