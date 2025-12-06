c1cc7211b7107f01ca223f0f0ac11584310ce6ea
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-06 15:50:12 +0530

    Update productRouter.js

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,5 @@
 import express from 'express'; // Import express
-import { createProduct, deleteProduct, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router(); // Create a router instance from express
 
@@ -7,5 +7,6 @@ productRouter.get("/",getProducts); // Route to get all products
 productRouter.post("/",createProduct); // Route to create a new product
 productRouter.delete("/:productId", deleteProduct); // Route to delete a product by ID
 productRouter.put("/:productId", updateProduct); // Route to update a product by ID
+productRouter.get("/:productId", getProductById); // Route to get a product by ID
 
 export default productRouter; // Export the router
