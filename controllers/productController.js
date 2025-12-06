8315afcea93a9a56ca86989528ad8e54757e857a
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-06 12:20:08 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,15 @@ import Product from "../models/product.js"; // Import Product model
 
 // Handle product creation: Save product to database
 export async function createProduct(req, res) {
+
+    if(req.user == null){
+        res.status(401).json( 
+            {
+                message: "Please login to create a product and try again."
+            }
+        )
+        return;   // Stop further processing because user is not authenticated...
+    }
     try{
         const productData = req.body;  // Get product data from request body
         const product = new Product(productData);        // Create new Product instance by using the model
