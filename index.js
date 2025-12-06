a0094a537063f4c81f6907972fce22f378f37c45
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-06 19:08:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,6 @@ app.use("/products", productRouter);
 
 app.listen(5000,
     () => {
-        console.log("Server is started...")
-        console.log("You can proceed to your work...")
+        console.log("Server is running on port 5000")
     }
 )
