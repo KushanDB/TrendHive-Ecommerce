b46d9b722cf7d6936f719f8762a5974ab66dbd9b
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-07 15:09:00 +0530

    Add product card styling and update component
    Created productcard.css with styles for the product card and image. Updated ProductCard component to use new class names, import the CSS file, and adjust heading level for better structure.

--- TrendHive-Frontend/src/components/productCard.jsx
+++ TrendHive-Frontend/src/components/productCard.jsx
@@ -1,9 +1,11 @@
+import "./productCard.css"
+
 export default function ProductCard() {
   return (
-    <div className="product-card">
-      <h2>Apple iPad</h2>
+    <div className="productCard">
+      <h1>Apple iPad</h1>
       <p>Price: $499.99</p>
-      <img src ="https://www.apple.com/assets-www/en_WW/ipad/01_product_tile/medium/ipad_air_a5aaa09ad_2x.jpg" alt="Apple iPad" />
+      <img className="productImg" src ="https://www.apple.com/assets-www/en_WW/ipad/01_product_tile/medium/ipad_air_a5aaa09ad_2x.jpg" alt="Apple iPad" />
       <button>Add to Cart</button>
     </div>
   )
