66c2a6ce7cf4cda3a5316092e04125a908081d09
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-07 15:00:25 +0530

    Add ProductCard component and update App.jsx
    Introduced a new ProductCard component displaying an Apple iPad with price and image. Updated App.jsx to render ProductCard instead of static content, removing unused imports and state.

--- TrendHive-Frontend/src/App.jsx
+++ TrendHive-Frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ProductCard from './components/productCard.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>TrendHive E-Commerce</h1>
+      <ProductCard/>
     </>
   )
 }
---
+++ TrendHive-Frontend/src/components/productCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+  return (
+    <div className="product-card">
+      <h2>Apple iPad</h2>
+      <p>Price: $499.99</p>
+      <img src ="https://www.apple.com/assets-www/en_WW/ipad/01_product_tile/medium/ipad_air_a5aaa09ad_2x.jpg" alt="Apple iPad" />
+      <button>Add to Cart</button>
+    </div>
+  )
+}
