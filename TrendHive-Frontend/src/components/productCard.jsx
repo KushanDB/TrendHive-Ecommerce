6429ac29c7143c0a81fe15819decae27c751b032
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-07 18:17:39 +0530

    Update productCard.jsx

--- TrendHive-Frontend/src/components/productCard.jsx
+++ TrendHive-Frontend/src/components/productCard.jsx
@@ -3,7 +3,7 @@ import "./productCard.css"
 export default function ProductCard(props) {  // props is like an object that holds all the attributes passed to the component
   return (
     <div className="productCard">
-      <h1>{props.name}</h1>
+      <h1>{props.name}</h1> {/* Accessing the 'name' attribute from props */}
       <p>Price: {props.price}</p>
       <img className="productImg" src ={props.image}/>
       <button>Add to Cart</button>
