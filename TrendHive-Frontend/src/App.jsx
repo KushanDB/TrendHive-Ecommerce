a0e5cd36bfa6712990c3513fc18e25d6a722208a
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-25 19:11:52 +0530

    Add admin page and set up routing in App.jsx
    Introduced a new AdminPage component and configured React Router in App.jsx to handle routes for home, register, and admin pages. Also removed the unused productcard.css file.

--- TrendHive-Frontend/src/App.jsx
+++ TrendHive-Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import AdminPage from './pages/adminPage.jsx';
 import ProductCard from './components/productCard.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <div className="w-full h-[80vh] bg-gray-600">
+        <Routes path="/">
+          <Route path='/' element={<h1>Home page</h1>}/>
+          <Route path='/register' element={<h1>Register page</h1>}/>
+          <Route path='/admin/*' element={<AdminPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ TrendHive-Frontend/src/pages/adminPage.jsx
@@ -0,0 +1,13 @@
+export default function AdminPage() {
+    return (
+        <div className="w-full h-full flex bg-blue-200 p-2">
+            <div className="w-[300px] h-full bg-blue-200">
+
+            </div>
+            <div className="w-[calc(100%-300px)] h-full bg-blue-800 rounded-[20px]">
+
+            </div>
+
+        </div>
+    );
+}
