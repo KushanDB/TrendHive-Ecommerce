f1e7c405ccc6c5d06631ed847a1094d425befd84
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-29 16:29:16 +0530

    Update header.jsx

--- TrendHive-Frontend/src/components/header.jsx
+++ TrendHive-Frontend/src/components/header.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
         <div className="w-full bg-accent h-[100px] text-gray-500 px-[40px] flex items-center justify-between">
             <div className="w-full h-full flex relative">
                 <img src="/img_12.png" className="h-[100%] absolute object-cover left-0" />
                 <div className="h-full flex justify-center items-center gap-[30px] ml-[50px] text-lg font-semibold w-full">
-                    <a href="/">Home</a>
-                    <a href="/products">Products</a>
-                    <a href="/about">About</a>
-                    <a href="/contact">Contact</a>
+
+                    <Link to="/">Home</Link>
+                    <Link to="/products">Products</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
                 </div>
             </div>
-
         </div>
     )
 }
