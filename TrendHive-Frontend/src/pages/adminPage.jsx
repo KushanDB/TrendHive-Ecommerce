31355fff0fba144dd341feaca2c66cf696ff794f
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-12-30 13:55:05 +0530

    Update adminPage.jsx

--- TrendHive-Frontend/src/pages/adminPage.jsx
+++ TrendHive-Frontend/src/pages/adminPage.jsx
@@ -1,13 +1,38 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import { FaChartLine } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { RiDropboxLine } from "react-icons/ri";
+import { RiShieldUserLine } from "react-icons/ri";
+
 
 export default function AdminPage() {
     return (
         <div className="w-full h-full flex bg-primary p-2">
-            <div className="w-[300px] h-full bg-primary flex flex-col items-center gap-[20px]">
-                <div className="flex flex-row w-[90%] h-[70px] bg-accent items-center rounded-2xl">
+            <div className="w-[300px] h-full bg-primary flex flex-col items-center gap-[10px]">
+                <div className="flex flex-row w-[90%] h-[70px] bg-accent items-center rounded-2xl mb-[10px]">
                     <img src="/img_12.png" alt="TrendHive Logo" className="h-[60px] m-2" />
                     <span className="text-secondary font-medium text-xl">Admin Panel</span>
                 </div>
+                <Link to="/admin" className="w-[90%] flex items-center gap-3 px-4 py-2 rounded hover:bg-secondary transition-colors text-accent font-medium">
+                    <FaChartLine/>
+                    Dashboard
+                </Link>
+
+                <Link to="/admin/orders" className="w-[90%] flex items-center gap-2 px-4 py-2 rounded hover:bg-secondary transition-colors text-accent font-medium">
+                    <MdOutlineShoppingCart className="text-xl"/>
+                    Orders
+                </Link>
+
+                <Link to="/admin/products" className="w-[90%] flex items-center gap-2 px-4 py-2 rounded hover:bg-secondary transition-colors text-accent font-medium">
+                    <RiDropboxLine className="text-lg" />
+                    Products
+                </Link>
+
+                <Link to="/admin/users" className="w-[90%] flex items-center gap-2 px-4 py-2 rounded hover:bg-secondary transition-colors text-accent font-medium">
+                    <RiShieldUserLine className="text-lg" />
+                    Users
+                </Link>
+
             </div>
 
 
